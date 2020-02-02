0cb1092c1b352ae7030e6bb47392d17bbec9b35d
EC2 Default User <ec2-user@ip-172-31-37-252.ap-northeast-1.compute.internal>
2020-02-02 10:29:55 +0000

    Implement advanced-login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -21,7 +21,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     post login_path, params:{session:{email: @user.email,
                          password:'password'}}
     assert is_logged_in?  # テストユーザーがログイン中の場合にtrueを返す
-    assert_redirected_to @user
+    assert_redirected_to @user #リダイレクト先が@userであることを確認する
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]",login_path,count:0
@@ -30,9 +30,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]",login_path
     assert_select "a[href=?]",logout_path, count:0
     assert_select "a[href=?]",user_path(@user), count:0
   end  
+  
+  test "login with remembering" do
+    log_in_as(@user,remember_me:'1')
+    assert_equal cookies['remember_token'],assigns(:user).remember_token
+  end
+  
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user,remember_me:'1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user,remember_me:'0')
+    assert_empty cookies['remember_token']
+  end
 end
