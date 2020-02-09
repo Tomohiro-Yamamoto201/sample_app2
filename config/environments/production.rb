ca340ba6f99dce925af907d41c56186687bbbdcf
EC2 Default User <ec2-user@ip-172-31-37-252.ap-northeast-1.compute.internal>
2020-02-09 07:01:16 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,11 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app2_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'https://agile-caverns-89961.herokuapp.com/'
-  config.action_mailer.default_url_options = [host: host]
+  config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = [
     :address => 'smtp.sendgrid.net',
     :port => '587',
