66575b2b362b5510c67d3f5f7ee5f573505bf29d
EC2 Default User <ec2-user@ip-172-31-37-252.ap-northeast-1.compute.internal>
2020-02-09 05:17:33 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '26f96af06e1c4080b5b7d0227e8ba69d.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host,protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
